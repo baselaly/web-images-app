8ec16ac48aed7bdaa73c95d99f68fbd712459754
basel <basel.3ly@gmail.com>
2020-06-12 06:59:21 +0300

    ivon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>SOLO</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('images/icon.jpg')}}" />
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
 </head>
 <body>
