638d390b33f46855e916591808e8056b15fafd80
basel <basel.3ly@gmail.com>
2020-06-23 16:08:29 +0300

    notification in create post

--- app/Observers/LikeObserver.php
+++ app/Observers/LikeObserver.php
@@ -11,7 +11,7 @@ class LikeObserver
     public function created(Like $like)
     {
         $likeable = $like->likeable;
-        if ($likeable instanceof \App\Post) {
+        if ($likeable instanceof \App\Post && $like->user_id != $likeable->user_id) {
             $body = $like->user->fullname . " Liked Your Post";
             $notification = (new SendNotification($body))->delay(Carbon::now()->addSeconds(5));
             $like->user->notify($notification);
--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -2,7 +2,11 @@
 
 namespace App\Observers;
 
+use App\Notifications\SendNotification;
 use App\Post;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
 
 class PostObserver
@@ -12,5 +16,12 @@ class PostObserver
         $postImages = $post->images;
         foreach ($postImages as $postImage)
             Storage::disk('images')->delete($postImage->getOriginal('image'));
+    }
+
+    public function created(Post $post)
+    {
+        $followers = User::whereIn('id', $post->user->followers->pluck('follower_id')->toArray())->get();
+        $body = $post->user->fullname . " Publish New Post";
+        Notification::send($followers, (new SendNotification($body))->delay(Carbon::now()->addSeconds(2)));
     }
 }
